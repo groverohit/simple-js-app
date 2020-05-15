d8892b30a8485350f50923b0dfcc51c25e8728c6
Rohit Grover <groverohit@gmail.com>
2020-05-15 11:33:37 -0700

    Intoduced loops and conditionals and added styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 var pokemonList = [];
 
 pokemonList = [
-  { name: 'Bulbasaur', height: '0.7', types: ['grass', 'poison']},
-  { name: 'Ivysaur', height: '1', types: ['grass', 'poison']},
-  { name: 'Venusaur', height: '2', types: ['grass', 'poison']},
-  { name: 'Charmander', height: '0.6', types: ['fire']}
+  { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+  { name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
+  { name: 'Venusaur', height: 2, types: ['grass', 'poison']},
+  { name: 'Charmander', height: 0.6, types: ['fire']}
   ]
 
+for (var i = 0; i < 4; i++) {
+  document.write('"' + (pokemonList[i].name) + ' (height:' + pokemonList[i].height + 'm)"');
+  pokemonList[i].height > 1? document.write(' - Wow, that’s big<br>'): document.write('<br>');
+}
