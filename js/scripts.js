f612929c31030c25047ce701523d369f41d5875a
Rohit Grover <groverohit@gmail.com>
2020-05-16 06:46:43 -0700

    Introduced functional programming paradigm

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,32 @@
-var pokemonList = [];
+var pokemonRepository = (function(){
 
-//an array containing pokemon obects with name, height and types
-pokemonList = [
+  //an array containing pokemon obects with name, height and types
+  var pokemonList = [
   { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
   { name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
   { name: 'Venusaur', height: 2, types: ['grass', 'poison']},
   { name: 'Charmander', height: 0.6, types: ['fire']}
   ]
 
-//iterate over pokemons in the list using for loop
-/*for (var i = 0; i < 4; i++) {
-  document.write('"' + (pokemonList[i].name) + ' (height:' + pokemonList[i].height + 'm)"');
-  pokemonList[i].height > 1? document.write(' - Wow, that’s big<br>'): document.write('<br>');
-}*/
+  //function to add pokemons to array
+  function add(pokemon){
+    pokemonList.push(pokemon);
+  }
+
+  //fgunction to return pokemons list
+  function getAll(){
+    return pokemonList;
+  }
+
+  return{
+    add: add,
+    getAll: getAll
+  }
+
+})();
 
 //iterate over pokemons in the list using forEach
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
   document.write('"' + (pokemon.name) + ' (height:' + pokemon.height + 'm)"');
   pokemon.height > 1? document.write(' - Wow, that’s big<br>'): document.write('<br>');
 });
