6fccf68931f460d55c669c41bf1a07fee9cf6eb1
Rohit Grover <groverohit@gmail.com>
2020-05-15 20:29:39 -0700

    Introduced forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var pokemonList = [];
 
+//an array containing pokemon obects with name, height and types
 pokemonList = [
   { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
   { name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
@@ -7,7 +8,14 @@ pokemonList = [
   { name: 'Charmander', height: 0.6, types: ['fire']}
   ]
 
-for (var i = 0; i < 4; i++) {
+//iterate over pokemons in the list using for loop
+/*for (var i = 0; i < 4; i++) {
   document.write('"' + (pokemonList[i].name) + ' (height:' + pokemonList[i].height + 'm)"');
   pokemonList[i].height > 1? document.write(' - Wow, that’s big<br>'): document.write('<br>');
-}
+}*/
+
+//iterate over pokemons in the list using forEach
+pokemonList.forEach(function(pokemon) {
+  document.write('"' + (pokemon.name) + ' (height:' + pokemon.height + 'm)"');
+  pokemon.height > 1? document.write(' - Wow, that’s big<br>'): document.write('<br>');
+});
