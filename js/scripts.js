f881a52e1d483d7a4350022633328c853a3896d9
Rohit Grover <groverohit@gmail.com>
2020-05-17 05:03:16 -0700

    Introduced DOM interaction

--- js/scripts.js
+++ js/scripts.js
@@ -8,25 +8,48 @@ var pokemonRepository = (function(){
   { name: 'Charmander', height: 0.6, types: ['fire']}
   ]
 
+  //function to log selected pokemon name
+  function showDetails(pokemon){
+    console.log(pokemon.name);
+  }
+
+  //function addListItem to add pokemons as list
+  function addListItem(pokemon){
+    var list = document.querySelector('.pokemon-list');
+    var listItem = document.createElement('li');
+    var button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('button-class');
+    listItem.appendChild(button);
+    list.appendChild(listItem);
+
+    //event listener for button clicks
+    button.addEventListener('click', function(event){
+      showDetails(pokemon);
+    });
+  }
+
   //function to add pokemons to array
   function add(pokemon){
     pokemonList.push(pokemon);
   }
 
-  //fgunction to return pokemons list
+  //function to return pokemons list
   function getAll(){
     return pokemonList;
   }
 
+  //make functions accessible from outside the object
   return{
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails
   }
 
 })();
 
-//iterate over pokemons in the list using forEach
+//add each pokemon as list item
 pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write('"' + (pokemon.name) + ' (height:' + pokemon.height + 'm)"');
-  pokemon.height > 1? document.write(' - Wow, that’s big<br>'): document.write('<br>');
+  pokemonRepository.addListItem(pokemon);
 });
